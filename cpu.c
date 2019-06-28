b243080a2193f60876df2d5da7cfd748e06ca63d
Elisha Atulomah <elishaubax@gmail.com>
2019-06-28 11:23:13 -0700

    Copy updated source file

--- cpu.c
+++ cpu.c
@@ -1,20 +1,16 @@
-#include "cpu.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "cpu.h"
 
 /**
- * Read RAM inside CPU struct at given index
+ * RAM 
  */
-unsigned char cpu_ram_read(struct cpu *cpu, unsigned int index)
+unsigned char cpu_ram_read(struct cpu *cpu, unsigned char index)
 {
     return cpu->ram[index];
 }
-
-/**
- * Write a value to the RAM inside CPU struct at given index
- */
-void cpu_ram_write(struct cpu *cpu, unsigned int index, unsigned char value)
+void cpu_ram_write(struct cpu *cpu, unsigned char index, unsigned char value)
 {
     cpu->ram[index] = value;
 }
@@ -24,11 +20,10 @@ void cpu_ram_write(struct cpu *cpu, unsigned int index, unsigned char value)
  */
 void cpu_load(struct cpu *cpu, char *file)
 {
-
     FILE *fp;
     char line[128];
-
     int address = 0;
+
     fp = fopen(file, "r");
     if (fp == NULL)
     {
@@ -44,22 +39,121 @@ void cpu_load(struct cpu *cpu, char *file)
         {
             continue;
         }
-
-        cpu->ram[address++] = value;
+        cpu_ram_write(cpu, address++, value);
     }
+
     fclose(fp);
+}
+
+/**
+ * GEN handler functions 
+ */
+void gen(struct cpu *cpu, unsigned int op, unsigned char regA, unsigned char regB)
+{
+    switch (op)
+    {
+    case HLT:
+        break;
+    case LDI:
+        cpu->reg[regA] = regB;
+        break;
+    case ST:
+        cpu->ram[cpu->reg[regA]] = cpu->reg[regB];
+        break;
+    case PUSH:
+        cpu->reg[7]--;
+        cpu_ram_write(cpu, cpu->reg[7], cpu->reg[regA]);
+        break;
+    case POP:
+        cpu->reg[regA] = cpu_ram_read(cpu, cpu->reg[7]);
+        cpu->reg[7]++;
+        break;
+    case PRN:
+        printf("%d\n", cpu->reg[regA]);
+        break;
+    default:
+        fprintf(stderr, "Error - GEN Instruction Unknown {%d}\n", op);
+        exit(1);
+        break;
+    }
+}
+
+/**
+ * PC handler functions 
+ */
+void pc(struct cpu *cpu, unsigned int op, unsigned char regA)
+{
+    switch (op)
+    {
+    case CALL:
+        cpu->reg[7]--;
+        cpu_ram_write(cpu, cpu->reg[7], cpu->pc + 2);
+        cpu->pc = cpu->reg[regA];
+        break;
+    case RET:
+        cpu->pc = cpu_ram_read(cpu, cpu->reg[7]);
+        cpu->reg[7]++;
+        break;
+    case JMP:
+        cpu->pc = cpu->reg[regA];
+        break;
+    case JEQ:
+        if (cpu->fl & ETF)
+        {
+            cpu->pc = cpu->reg[regA];
+        }
+        else
+        {
+            cpu->pc += 2;
+        }
+        break;
+    case JNE:
+        break;
+    default:
+        fprintf(stderr, "Error - PC Instruction Unknown {%d}\n", op);
+        exit(1);
+        break;
+    }
 }
 
 /**
  * ALU
  */
-void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
+void alu(struct cpu *cpu, unsigned int op, unsigned char regA, unsigned char regB)
 {
     switch (op)
     {
-    case ALU_MUL:
-        // TODO
-        cpu->ram[regA] = cpu->ram[regA] * cpu->ram[regB];
+    case ADD:
+        cpu->reg[regA] = cpu->reg[regA] + cpu->reg[regB];
+        break;
+    case SUB:
+        cpu->reg[regA] = cpu->reg[regA] - cpu->reg[regB];
+        break;
+    case MUL:
+        cpu->reg[regA] = cpu->reg[regA] * cpu->reg[regB];
+        break;
+    case DIV:
+        cpu->reg[regA] = cpu->reg[regA] / cpu->reg[regB];
+        break;
+    case CMP:
+        // setting flags directly without comparision operator because two numbers can ONLY
+        // be <, >, OR =. Never both. Therefore only one flag will set to 1 and all others 0.
+        if (cpu->reg[regA] < cpu->reg[regB])
+        {
+            cpu->fl = LTF;
+        }
+        else if (cpu->reg[regA] > cpu->reg[regB])
+        {
+            cpu->fl = GTF;
+        }
+        else
+        {
+            cpu->fl = ETF;
+        }
+        break;
+    default:
+        fprintf(stderr, "Error - ALU Instruction Unknown {%d}\n", op);
+        exit(1);
         break;
     }
 }
@@ -73,44 +167,51 @@ void cpu_run(struct cpu *cpu)
 
     while (running)
     {
-        // TODO
         // 1. Get the value of the current instruction (in address PC).
         unsigned char IR = cpu_ram_read(cpu, cpu->pc);
+
         // 2. Figure out how many operands this next instruction requires
         int operands = IR >> 6;
+
         // 3. Get the appropriate value(s) of the operands following this instruction
-        unsigned char operandA, operandB;
-        if (operands > 0)
-        {
-            operandA = cpu_ram_read(cpu, cpu->pc + 1);
-            if (operands > 1)
-            {
-                operandB = cpu_ram_read(cpu, cpu->pc + 2);
-            }
-        }
+        unsigned char operandA = operands > 0 ? cpu_ram_read(cpu, (cpu->pc + 1) & 0xff) : 0;
+        unsigned char operandB = operands > 1 ? cpu_ram_read(cpu, (cpu->pc + 2) & 0xff) : 0;
+
+        // 0bAABCDDDD where AA is operands, B is if alu, C if PC, D is instruction identifier
+        int instruction_type = (IR >> 4) & 0b0011; // 0b0000->GEN; 0b0001->PC; 0b0010->ALU
+
         // 4. switch() over it to decide on a course of action.
-        switch (IR)
-        // 5. Do whatever the instruction should do according to the spec.
+        switch (instruction_type)
         {
-        case LDI:
-            cpu_ram_write(cpu, operandA, operandB);
+        // 5. Do whatever the instruction should do according to the spec.
+        case GEN:
+            if (IR == HLT)
+            {
+                running = 0;
+                break;
+            }
+            gen(cpu, IR, operandA, operandB);
+            cpu->pc += operands + 1;
             break;
-        case PRN:
-            printf("%d\n", cpu_ram_read(cpu, operandA));
+        case PC:
+            pc(cpu, IR, operandA);
             break;
-        case MUL:
-            alu(cpu, ALU_MUL, operandA, operandB);
-            break;
-        case HLT:
-            running = 0;
+        case ALU:
+            if (IR == DIV && operandB == 0)
+            {
+                fprintf(stderr, "Error - Cannot divide by 0\n");
+                running = 0;
+                break;
+            }
+            alu(cpu, IR, operandA, operandB);
+            cpu->pc += operands + 1;
             break;
         default:
-            printf("Error - Instruction Register Unknown at register PC index {%d}\n", cpu->pc);
+            fprintf(stderr, "Error - Instruction Unknown {%d}\n", IR);
             running = 0;
             break;
         }
         // 6. Move the PC to the next instruction.
-        cpu->pc += operands + 1;
     }
 }
 
@@ -120,16 +221,18 @@ void cpu_run(struct cpu *cpu)
 void cpu_init(struct cpu *cpu)
 {
     // TODO: Initialize the PC and other special registers
+    // When the LS-8 is booted, the following steps occur:
 
     // * `R0`-`R6` are cleared to `0`.
-    memset(cpu->reg, 0, 8 * sizeof(unsigned char));
+    memset(cpu->reg, 0, sizeof(cpu->reg));
 
     // * `R7` is set to `0xF4`.
     cpu->reg[7] = 0xF4;
 
     // * `PC` and `FL` registers are cleared to `0`.
     cpu->pc = 0;
+    cpu->fl = 0;
 
     // * RAM is cleared to `0`.
-    memset(cpu->ram, 0, 256 * sizeof(unsigned char));
+    memset(cpu->ram, 0, sizeof(cpu->ram));
 }
--- cpu.h
+++ cpu.h
@@ -4,7 +4,9 @@
 // Holds all information about the CPU
 struct cpu
 {
-  unsigned int pc;
+  // PC & FL
+  unsigned char pc;
+  unsigned char fl;
   // registers (array) -- R0->R7 (8 items)
   unsigned char reg[8];
   // ram (array) -- With 8 bits, our CPU has a total of 256 bytes of memory
@@ -12,22 +14,41 @@ struct cpu
 };
 
 // ALU operations
-enum alu_op
-{
-  ALU_MUL
-  // Add more here
-};
+// enum alu_op {
+// 	ALU_MUL,
+//   ALU_ADD
+// };
 
 // Instructions
 
 // These use binary literals. If these aren't available with your compiler, hex
 // literals should be used.
 
-#define LDI 0b10000010
-#define PRN 0b01000111
-// TODO: more instructions here. These can be used in cpu_run().
+#define GEN 0b0000
 #define HLT 0b00000001
+#define LDI 0b10000010
+#define ST 0b10000100
+#define PUSH 0b01000101
+#define POP 0b01000110
+#define PRN 0b01000111
+
+#define PC 0b0001
+#define CALL 0b01010000
+#define RET 0b00010001
+#define JMP 0b01010100
+#define JEQ 0b01010101
+
+#define ALU 0b0010
+#define ADD 0b10100000
+#define SUB 0b10100001
 #define MUL 0b10100010
+#define DIV 0b10100011
+#define CMP 0b10100111
+
+// Flags
+#define LTF 0b00000100 // less than flag
+#define GTF 0b00000010 // greater than flag
+#define ETF 0b00000001 // equal to flag
 
 // Function declarations
 
